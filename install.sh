c51b72ee70168c4236147a6e157ac3cd376fe6bb
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2024-06-15 16:56:32 +0200

    Move to correct directory if needed during installation

--- install.sh
+++ install.sh
@@ -3,6 +3,16 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+if [[ "$1" != "--OK" ]]; then
+    if [[ "$(basename $SCRIPT_DIR)" != "scripts" ]]; then
+        mv $SCRIPT_DIR $SCRIPT_DIR-_tmp_
+        mkdir $SCRIPT_DIR
+        mv $SCRIPT_DIR-_tmp_ $SCRIPT_DIR/scripts
+        $SCRIPT_DIR/scripts/install.sh --OK
+        exit
+    fi
+fi
+
 cd $SCRIPT_DIR
 rm -Rf .venv
 python3 -m venv .venv
@@ -12,8 +22,8 @@ python3 command_entry.py
 
 echo Successful Installation
 echo
-echo You have successfully installed my-dockers. Run the following command
-echo to start the manager:
+echo You have successfully installed my-dockers. Edit the following file
+echo to add your commands:
 echo
-echo     my-dockers
+echo     `realpath $SCRIPT_DIR/../commands.yaml`
 echo
